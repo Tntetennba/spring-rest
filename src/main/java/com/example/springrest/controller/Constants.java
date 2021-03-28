6c3270c1fee3070eba2bc1a8afced0169085cb28
Sergei Kolotushkin <name.txt@gmail.com>
2021-03-28 15:25:43 +0300

    Initial commit

---
+++ src/main/java/com/example/springrest/controller/Constants.java
@@ -0,0 +1,10 @@
+package com.example.springrest.controller;
+
+public class Constants {
+    public static final String WORLD = "<note>\n" +
+            "<to>Tove</to>\n" +
+            "<from>Jani</from>\n" +
+            "<heading>Reminder</heading>\n" +
+            "<body>Don't forget me this weekend!</body>\n" +
+            "</note>";
+}
